148ba1224cd1007b5d33505f021d665cdeaa60c5
somedeveloperr <artemallaguloff@gmail.com>
2026-09-07 07:40:22 +0500

    Update SimpleStorage.sol
    feat: add SimpleStorage contract with events and public getter

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.8.20;
 
+/// @title Простой контракт для хранения числа
+/// @notice Позволяет сохранять и считывать число с эмиссией событий
 contract SimpleStorage {
-    uint256 private myNumber;
+    /// @notice Сохраненное значение
+    /// @dev Public автоматически создает getter-функцию myNumber()
+    uint256 public myNumber;
 
-    function setNumber(uint256 _number) public {
+    /// @notice Событие, вызываемое при изменении числа
+    event NumberUpdated(address indexed setter, uint256 newNumber);
+
+    /// @notice Сохраняет новое число в блокчейн
+    /// @param _number Новое значение для записи
+    function setNumber(uint256 _number) external {
         myNumber = _number;
-    }
-
-    function getNumber() public view returns (uint256) {
-        return myNumber;
+        emit NumberUpdated(msg.sender, _number);
     }
 }
