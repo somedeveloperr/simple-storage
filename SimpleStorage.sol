7092d532a4a68c93a77f6626d1640f84edc46109
somedeveloperr <artemallaguloff@gmail.com>
2026-09-15 18:25:40 +0500

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,17 +2,29 @@
 pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
-import "../contracts/SimpleStorage.sol";
+import "../src/SimpleStorage.sol";
 
 contract SimpleStorageTest is Test {
     SimpleStorage public storageContract;
+
+    event NumberUpdated(uint256 indexed newNumber);
 
     function setUp() public {
         storageContract = new SimpleStorage();
     }
 
+    // Юнит-тест с проверкой эмиссии событий
     function test_SetNumber() public {
+        vm.expectEmit(true, false, false, true);
+        emit NumberUpdated(42);
+        
         storageContract.setNumber(42);
         assertEq(storageContract.myNumber(), 42);
+    }
+
+    // Фуззинг: проверка на случайных значениях
+    function testFuzz_SetNumber(uint256 x) public {
+        storageContract.setNumber(x);
+        assertEq(storageContract.myNumber(), x);
     }
 }
